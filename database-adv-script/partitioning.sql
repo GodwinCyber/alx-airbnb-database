22bba60a87c8c511dbf066ddab331dc444d9af70
GodwinCyber <godwinchuks032@gmail.com>
2025-09-05 07:05:58 +0100

    Partitioning large table

--- database-adv-script/partitioning.sql
+++ database-adv-script/partitioning.sql
@@ -1,3 +1,5 @@
+-- PARTITION BY
+
 -- Assume the Booking table is large and query performance is slow. Implement partitioning on the Booking table based on the start_date column. Save the query in a file partitioning.sql
 
 -- If you already have an existing booking table, rename it to booking_old
